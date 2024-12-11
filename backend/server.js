0fdce583e2c6bc187e4ab7446f99018be88bbe89
dci-student <mmn.umfragen@gmail.com>
2024-12-11 15:24:42 +0100

    add cors and cookie-parser into server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(cookieParser());
+
+app.use(cors());
 
 app.use(express.json());
 
