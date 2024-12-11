2982f62cd4afabd5ae03ec34a3408f7b162bd9a9
dci-student <mmn.umfragen@gmail.com>
2024-12-11 13:58:26 +0100

    style with prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
+import "./App.css";
 
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 
-import { MantineProvider } from '@mantine/core';
-import TestComponent from './components/TestComponent';
+import { MantineProvider } from "@mantine/core";
+import TestComponent from "./components/TestComponent";
 
 export default function App() {
   return (
     <MantineProvider>
       <TestComponent />
     </MantineProvider>
-  )
+  );
 }
