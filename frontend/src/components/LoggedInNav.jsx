6874667d34067b16366fd9c0e6c4728f327922a2
Timo Baumeister <TimoBaumeister2403@gmail.com>
2024-12-17 15:36:32 +0100

    LoggedOutAndInNav

--- frontend/src/components/LoggedInNav.jsx
+++ frontend/src/components/LoggedInNav.jsx
@@ -1,34 +1,48 @@
-import { Navbar, Button, Group } from "@mantine/core";
+import React from "react";
+import { Header, Container, Group, Button, Menu, Text, Title } from "@mantine/core";
 
 const LoggedInNav = ({ onLogout }) => {
   return (
-
-
-
-
-    <Navbar height={60} p="md" style={{ display: "flex", justifyContent: "space-between" }}>
-      {/* Links */}
-      <Group>
-        <Button variant="subtle" color="blue">
-          Dashboard
-        </Button>
-        <Button variant="subtle" color="blue">
-          Profile
-        </Button>
-      </Group>
-
-
-
-
-
-      {/* Rechts */}
-      <Group>
-        <Button color="red" onClick={onLogout}>
-          Logout
-        </Button>
-      </Group>
-    </Navbar>
+    <Header height={70} px="md" style={{ backgroundColor: "lightblue" }}>
+      <Container
+        size="xl"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "100%",
+        }}>
+        
+        <Title order={3} style={{ fontWeight: "bold" }}>
+          pixel-together
+        </Title>
+        
+        <Group spacing="sm">
+          <Button variant="subtle" size="md">
+            Dashboard
+          </Button>
+          <Button variant="subtle" size="md">
+            Team
+          </Button>
+          <Menu>
+            <Menu.Target>
+              <Button variant="default" size="md">
+                Mein Konto
+              </Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item>Profil</Menu.Item>
+              <Menu.Item>Account-Einstellungen</Menu.Item>
+              <Menu.Item color="red" onClick={onLogout}>
+                Ausloggen
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
+      </Container>
+    </Header>
   );
 };
 
 export default LoggedInNav;
+
