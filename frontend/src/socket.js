d7cf1993e31eec54353c2780d2393e3b612cf16b
Timo Baumeister <TimoBaumeister2403@gmail.com>
2025-01-16 15:52:22 +0100

    FrontendSocket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,11 +1,9 @@
 import { io } from "socket.io-client";
 
-// Hole die Socket-URL aus der .env-Datei (oder verwende die Backend-URL als Fallback)
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || import.meta.env.VITE_API_BASE_URL;
 
-// Initialisiere die Socket.IO-Verbindung
 const socket = io(SOCKET_URL, {
-  autoConnect: false, // Verbindung nur bei Bedarf herstellen
+  autoConnect: false,
 });
 
 export default socket;
